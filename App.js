2fe6940d9ca75613675a5c602593c1f05aceca8e
Mateus Borges <mateusgaspio@gmail.com>
2022-06-07 04:21:18 -0400

    Começando os Styles

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Platform } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor='#fff' translucent={false} />
+
+      {/* <Image
+        source={require('./src/assets/logo_instagram.png')}
+        style={styles.logo}
+        
+      /> */}
+      
+      <TextInput
+        placeholder='Celular, username ou email'
+        style={styles.input}
+      />
+
+      <TextInput
+        placeholder='Sua Senha'
+        style={styles.input}
+      />
+
+      <View style={styles.forgotPass}>
+        <TouchableOpacity>
+          <Text style={styles.forgotPassText}>
+            Esqueceu sua senha?
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.loginButton}>
+        <Text styles={styles.loginText}>
+          Acessar
+        </Text>
+      </TouchableOpacity>
+
+      {/* Distancia Variavel do android e iOS */}
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 });
